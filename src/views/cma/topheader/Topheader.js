106431b4a7eac0a36914fe5fb664362916950fb4
Make-I-promise <3487799890@qq.com>
2020-03-23 17:02:29 +0800

    login页面和首页顶部头像-yyf-第一次上传

--- src/views/cma/topheader/Topheader.js
+++ src/views/cma/topheader/Topheader.js
@@ -1,27 +1,68 @@
 import React, { Component } from 'react'
-import { Layout } from 'antd';
+import { Layout, Menu, Dropdown, Avatar } from 'antd'
+import { withRouter } from 'react-router'
 import {
   MenuUnfoldOutlined,
   MenuFoldOutlined,
+  UserOutlined 
 } from '@ant-design/icons';
 const { Header } = Layout;
-export default class Topheader extends Component {
+
+class Topheader extends Component {
   state = {
     collapsed: false,
+    token: {}
   }
   toggle = () => {
     this.setState({
       collapsed: !this.state.collapsed,
-    });
+    })
+  }
+  componentDidMount () {
+    let token = localStorage.getItem('token')
+
+    this.setState({
+      token: JSON.parse(decodeURIComponent(window.atob(token)))
+    })
+  }
+  //下拉菜单
+  menu () {
+    return (
+        <Menu>
+            <Menu.Item>
+                <div>
+                    {this.state.token.roleName}
+                </div>
+            </Menu.Item>
+            <Menu.Item>
+                <div onClick={ this.signOut }>
+                    退出
+                </div>
+            </Menu.Item>
+        </Menu>
+    )
   }
   render() {
     return (
       <Header className="site-layout-background" style={{ padding: 0 }}>
-        {React.createElement(this.state.collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
-          className: 'trigger',
-          onClick: this.toggle,
-        })}
+          {React.createElement(this.state.collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
+            className: 'trigger',
+            onClick: this.toggle,
+          })}
+
+          <div style={ {float:'right',padding:'0 10px'} }>
+              欢迎{this.state.token.username}回来！
+              <Dropdown overlay={ this.menu() }>
+                  <Avatar size="large" src="https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=3705382457,1208323352&fm=26&gp=0.jpg" />
+              </Dropdown>
+          </div>
       </Header>
     )
   }
+  signOut = () => {
+    localStorage.setItem('token', '')
+    this.props.history.push('/login')
+  }
 }
+
+export default withRouter(Topheader)
