1d34495665449cc596954212ab63b2883840f8fd
Lio0113 <865763975@qq.com>
2020-03-23 15:17:26 +0800

    lz-权限列表

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,19 @@
-import React,{Component} from 'react'
-import {HashRouter as Router,Route,Switch,Redirect} from 'react-router-dom'
+import React, { Component } from 'react'
+import { HashRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
 import Blog from "../views/blog/Blog"
 import Cma from '../views/cma/Cma'
 import Login from '../views/login/Login'
 export default class BlogRouter extends Component {
-  render () {
+  render() {
     return (
       <Router>
         <Switch>
           <Route path='/login' component={Login}></Route>
-          <Route path='/cma' render={()=>{
+          <Route path='/cma' render={() => {
             return (
-              localStorage.getItem('token')?
-              <Cma></Cma>
-              :<Redirect to='/login'></Redirect>
+              localStorage.getItem('token') ?
+                <Cma></Cma>
+                : <Redirect to='/login'></Redirect>
             )
           }}></Route>
           <Route path='/' component={Blog}></Route>
--- src/views/cma/rightManage/Right/Right.js
+++ src/views/cma/rightManage/Right/Right.js
@@ -1,10 +1,50 @@
 import React, { Component } from 'react'
 import './right.css'
+import { Table, Tag } from 'antd';
 export default class Right extends Component {
+
+  componentDidMount() {
+    React.$axios.get("http://localhost:12138/rights").then(res => {
+      // console.log(res.data);
+      this.setState({
+        data: res.data
+      })
+    })
+  }
+
+  state = {
+    columns: [
+      {
+        title: '#',
+        dataIndex: 'id',
+        key: 'id',
+      },
+      {
+        title: '权限名称',
+        dataIndex: 'title',
+        key: 'title'
+      },
+      {
+        title: '权限等级',
+        dataIndex: 'grade',
+        key: 'grade',
+        render: item => {
+          var arr = ["green", "blue", "volcano"]
+          return (
+            <Tag color={arr[item - 1]}>{item}</Tag>
+          )
+        }
+      }
+    ],
+    data: []
+
+  }
+
   render() {
     return (
       <div>
-        right
+        <Table columns={this.state.columns} dataSource={this.state.data}
+          pagination={{ pageSize: 5 }} />
       </div>
     )
   }
