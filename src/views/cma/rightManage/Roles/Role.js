8679710c593d267602374e34c272a4aec45a767d
qiaowanze <l>
2020-03-23 18:04:39 +0800

    qwz-第二次提交，优化页面，避免数据重复请求

--- src/views/cma/rightManage/Roles/Role.js
+++ src/views/cma/rightManage/Roles/Role.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react'
-import { Table,Button ,Tag} from 'antd';
+import { Table, Button, Tag } from 'antd';
 import './role.css';
 import { SearchOutlined } from '@ant-design/icons';
-
+import store from '../../../../redux/store'
 export default class Role extends Component {
     state = {
         columns: [
@@ -12,19 +12,39 @@ export default class Role extends Component {
                 dataIndex: '',
                 key: 'x',
                 render: () => <div>
-                          <Button type="primary" shape="circle" icon={<SearchOutlined />} disabled={true}/>
+                    <Button type="primary" shape="circle" icon={<SearchOutlined />} disabled={true} />
                 </div>,
             }
         ],
         datalist: []
 
     }
+    actionCreate = () => {
+        return (dispatch) => {
+            React.$axios.get("http://localhost:12138/roles").then(res => {
+                dispatch({
+                    type: 'role',
+                    payload: res.data
+                })
+            })
+        }
+    }
     componentDidMount() {
-      React.$axios.get("http://localhost:12138/roles").then(res => {
+        if (store.getState().roleList.length === 0) {
+            store.dispatch(this.actionCreate())
+        } else {
             this.setState({
-                datalist: res.data
+                datalist: store.getState().roleList
+            })
+        }
+        this.unscribe = store.subscribe(() => {
+            this.setState({
+                datalist: store.getState().roleList
             })
         })
+    }
+    componentWillUnmount() {
+        this.unscribe();
     }
 
     render() {
@@ -32,26 +52,24 @@ export default class Role extends Component {
             <Table
                 columns={this.state.columns}
                 dataSource={this.state.datalist}
-                rowKey= {item=>{
+                rowKey={item => {
                     return item.id
                 }}
-                
-
                 expandable={{
                     expandedRowRender: record => {
                         return <div style={{ margin: 0 }}>
-                        {
-                            record.roleRight.map(item=>
-                               <div key={item.category}>
-                                   {
-                                       item.list.map(data=>
-                                        <Tag color={"green"} key={data}>{data}</Tag>
-                                        )
-                                   }
-                               </div> 
-                            )
-                        }
-                    </div>
+                            {
+                                record.roleRight.map(item =>
+                                    <div key={item.category}>
+                                        {
+                                            item.list.map(data =>
+                                                <Tag color={"green"} key={data}>{data}</Tag>
+                                            )
+                                        }
+                                    </div>
+                                )
+                            }
+                        </div>
                     },
                 }}
             />
