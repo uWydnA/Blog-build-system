756432481e202e4f6fe421b1bd58e19b39ae2bf3
Lio0113 <865763975@qq.com>
2020-03-23 22:28:29 +0800

    lz-权限列表

--- src/views/cma/rightManage/Right/Right.js
+++ src/views/cma/rightManage/Right/Right.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import './right.css'
-import { Table, Tag } from 'antd';
+import { Table, Tag, Button, Modal, Select, Form } from 'antd';
+import { EditOutlined } from '@ant-design/icons';
+const { Option } = Select
 export default class Right extends Component {
 
   componentDidMount() {
@@ -13,6 +15,9 @@ export default class Right extends Component {
   }
 
   state = {
+    myGrade: '',
+    formdata: "",
+    visible: false,
     columns: [
       {
         title: '#',
@@ -34,10 +39,60 @@ export default class Right extends Component {
             <Tag color={arr[item - 1]}>{item}</Tag>
           )
         }
+      },
+      {
+        title: '修改权限',
+        dataIndex: '',
+        key: 'x',
+        render: (item) => <div>
+          <Button type="primary" shape="circle" icon={<EditOutlined />} disabled={item.default} onClick={() => { this.update(item.id) }} />
+        </div>
       }
     ],
     data: []
+  }
 
+  update = (data) => {
+    var formdata = this.state.data.filter(item => item.id === data)
+    // console.log(formdata[0]);
+    this.setState({
+      formdata: formdata[0],
+      visible: true,
+      myGrade: formdata[0].grade
+    })
+    let { grade } = formdata[0]
+    this.refs.updateText && this.refs.updateText.setFieldsValue({
+      grade
+    })
+  }
+
+  onChange = (value) => {
+    console.log(value);
+    this.setState({
+      myGrade: value
+    })
+  }
+
+  handleUpdateOk = () => {
+    this.refs.updateText.validateFields().then(value => {
+      // console.log(value, this.state.formdata);
+      React.$axios.put(`http://localhost:12138/rights/${this.state.formdata.id}`, {
+        ...this.state.formdata,
+        ...value
+      }).then(res => {
+        var newlist = this.state.data.map(item => {
+          if (item.id === this.state.formdata.id) {
+            return res.data
+          } else {
+            return item
+          }
+        })
+        this.setState({
+          visible: false,
+          data: newlist
+        })
+      })
+    })
   }
 
   render() {
@@ -45,6 +100,42 @@ export default class Right extends Component {
       <div>
         <Table columns={this.state.columns} dataSource={this.state.data}
           pagination={{ pageSize: 5 }} />
+        <Modal
+          title="更新用户"
+          visible={this.state.visible}
+          onOk={this.handleUpdateOk}
+          okText="提交"
+          cancelText="取消"
+          onCancel={() => {
+            this.setState({
+              visible: false
+            })
+          }}
+        >
+          <Form
+            layout="vertical"
+            name="form_in_modal"
+            ref="updateText"
+            initialValues={this.state.formdata}>
+            <Form.Item name="grade" label="权限等级"
+              rules={[{ required: true, message: 'Please input the title of collection!' }]}>
+              <Select
+                showSearch
+                placeholder="请选择权限等级"
+                optionFilterProp="children"
+                onChange={this.onChange}
+                onSearch={this.onSearch}
+                filterOption={(input, option) =>
+                  option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+                }
+              >
+                <Option value="1">1</Option>
+                <Option value="2">2</Option>
+                <Option value="3">3</Option>
+              </Select>
+            </Form.Item>
+          </Form>
+        </Modal>
       </div>
     )
   }
